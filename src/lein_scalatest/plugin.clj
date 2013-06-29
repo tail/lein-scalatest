b3cc5925f540003ef24a98623eabc974de1135a5
Aaron Bernard <aaron.bernard@gmail.com>
2013-06-29 16:10:45 -0500

    scalatest version settable via :scalatest-version property

--- src/lein_scalatest/plugin.clj
+++ src/lein_scalatest/plugin.clj
@@ -1,7 +1,12 @@
 (ns lein-scalatest.plugin)
 
+(defn- version
+  [project]
+  (or (:scalatest-version project) "2.0.M5b"))
+
 (defn middleware [project]
   (-> project
     (update-in [:dependencies] concat
                [['com.duramec/lein-scalac "0.1.1"]
-                ['org.scalatest/scalatest_2.10 "2.0.M5b"]])))
+                ['org.scalatest/scalatest_2.10 (version project)]])))
+
