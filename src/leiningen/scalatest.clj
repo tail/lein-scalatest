7865eb6e390ce514a557859c060e24b833d9c273
Michael Maltese <michaelm@disqus.com>
2014-08-08 14:28:03 -0700

    Modifications for Disqus

--- src/leiningen/scalatest.clj
+++ src/leiningen/scalatest.clj
@@ -1,30 +1,42 @@
 (ns leiningen.scalatest
   (:require [leiningen.core.main :as lein]
             [leiningen.core.eval :as eval]
+            [leiningen.compile :as compile]
+            [leiningen.jar :as jar]
             [leiningen.scalac :as scalac]))
 
-(defn- scalatest-props
-  [project]
-  (let [runpath (:compile-path project)]
-    ["-R" runpath 
-     "-o"]))
+(defn- scalatest-xunit-path []
+  (or (System/getenv "XUNIT_RESULTS_PATH") "test_results/xunit"))
+
+(defn- scalatest-props [project]
+  ["-R" (:compile-path project) "-o" "-u" (scalatest-xunit-path)])
 
 (defn- scalatest-form
   [project]
   `(do
      (import org.scalatest.tools.Runner)
-     (if (org.scalatest.tools.Runner/run (into-array String ~(scalatest-props project)))
-       (System/exit 0)
-       (System/exit 1))))
-
-(defn- scalac-props
-  [project]
-  (update-in project [:scala-source-paths] concat (:scala-test-paths project)))
+     (let [xunit-path# ~(scalatest-xunit-path)
+           ret# (org.scalatest.tools.Runner/run (into-array String ~(scalatest-props project)))]
+        ; Jenkins-xUnit does not like the output from ScalaTest, so fix it
+        (doseq [junit# (.listFiles (clojure.java.io/file xunit-path#))]
+          (if-not (.isDirectory junit#)
+            (spit junit# (clojure.string/replace (slurp junit#) #">\s*</property>" "/>"))))
+        (if ret#
+          (System/exit 0)
+          (System/exit 1))
+      )))
 
 (defn scalatest
   "Run scalatest on tests in :scala-test-paths"
   [project & args]
   (if (not (:scala-test-paths project))
-    (lein/abort "lein scalatest: You must specify :scala-test-paths [] in your project.clj"))
-  (scalac/scalac (scalac-props project)) 
-  (eval/eval-in-project project (scalatest-form project)))
+    (lein/abort "lein scalatestc: You must specify :scala-test-paths [] in your project.clj"))
+  (let [project (assoc project
+      :dependencies
+        (apply concat (for [k [:test :provided]] (get-in project [:profiles k :dependencies])))
+      :resource-paths
+        [(jar/get-jar-filename project :standalone)]
+      :prep-tasks [])]
+    (doseq [path (:scala-test-paths project)]
+      (scalac/scalac (assoc project :scala-source-path path)))
+    (eval/eval-in-project project (scalatest-form project))))
